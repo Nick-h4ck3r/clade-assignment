76a39c00f6f1a832fefc0757d3ea384c7d1559cb
Nikhil Kadam <nickkadam23@gmail.com>
2024-06-28 22:00:06 +0530

    feat: job details component

--- app/jobs/[jobId]/page.tsx
+++ app/jobs/[jobId]/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import JobTabs from "@/components/JobTabs";
 import JobTitle from "@/components/JobTitle";
+import JobDetails from "@/components/JobDetails";
 
 type Props = {};
 
@@ -10,10 +11,13 @@ const JobPage = (props: Props) => {
       <div className="border">
         <JobTabs />
       </div>
+
       <div className="flex px-16 text-sm">
         <div className="flex flex-col bg-white w-3/4 border-r py-4">
           <JobTitle />
+          <JobDetails />
         </div>
+
         <div className="flex flex-col bg-white w-1/4 px-4 py-4">qwe</div>
       </div>
     </>
---
+++ components/JobDetails.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+type Props = {};
+
+const JobDetails = (props: Props) => {
+  return (
+    <div className="border-b-2">
+      <div className="grid grid-cols-4 py-6 w-fit *:border-gray-300 *:text-sm *:pr-10 *:flex *:flex-col *:gap-3">
+        <div>
+          <p className="text-xs font-medium text-gray-600">Skills Required</p>
+          <div className="flex flex-col gap-2">
+            <p className="text-xs border w-fit p-1.5 rounded-md">Figma</p>
+            <p className="text-xs border w-fit p-1.5 rounded-md">
+              Adobe Illustrator
+            </p>
+            <p className="text-xs border w-fit p-1.5 rounded-md">Adobe XD</p>
+          </div>
+        </div>
+
+        <div>
+          <p className="text-xs font-medium text-gray-600">
+            Preferred Language
+          </p>
+          <p className="font-bold text-base">English</p>
+        </div>
+        <div>
+          <p className="text-xs font-medium text-gray-600">Type</p>
+          <p className="font-bold text-base">Full-time</p>
+        </div>
+        <div>
+          <p className="text-xs font-medium text-gray-600">
+            Years of Experience
+          </p>
+          <p className="font-bold text-base">3+ Years of Experience</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JobDetails;
